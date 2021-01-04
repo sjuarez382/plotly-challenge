701e433fb3945c4b7b983570028b95d7fc65583a
Serafin <serafinjuarez@yahoo.com>
2021-01-03 16:05:41 -0800

    input code

--- static/app.js
+++ static/app.js
@@ -1,4 +1,17 @@
 //Use the D3 library to read in samples.json
+d3.json("samples.json").then(function createPlotly(data) {
+    console.log(data);
+    var testid = data.names;
+    console.log(testid);
 
+    d3.select("#selDataset")
+    .selectAll("option")
+    .data(testid)
+    .enter()
+    .append("option")
+    .htlm(function(d) {
+        return `option>${d}</option>`;
+    });
+});
 //Create a horizontal bar chart with a dropdown menu to display the top 10 OTUs found in that individual.
 
