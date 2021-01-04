4dee67df0337bc83f61922adbb51601d8a1a20a4
Serafin <serafinjuarez@yahoo.com>
2021-01-03 16:12:34 -0800

    input code

--- static/app.js
+++ static/app.js
@@ -13,10 +13,20 @@ d3.json("samples.json").then(function createPlotly(data) {
         return `option>${d}</option>`;
     });
 
-    var dropdownMenu =
-    var dropdownValue =
-    var index = 
+    var dropdownMenu = d3.select("#selDataset");
+    var dropdownValue = dropdownMenu.property("value");
+    var index = testid.indedOf(dropdownValue);
 
+    d3.select("#sample-metadata").html("");
+    d3.select("#sample-metadata")
+        .selectAll("p")
+        .data(Object.defineProperties(data.metadata[index]))
+        .enter()
+        .append("p")
+        .html(function(d) {
+            return `${d[0]}:${d[1]}`;
+        });
+    console.log(Object.defineProperties(data.metadata[index]));
 });
 //Create a horizontal bar chart with a dropdown menu to display the top 10 OTUs found in that individual.
 
